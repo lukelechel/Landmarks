f35c46a2402815198e00b6545e04507b51d0a077
Luke Lechel <13129882+lukelechel@users.noreply.github.com>
2022-03-17 13:26:31 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Luke Lechel on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
